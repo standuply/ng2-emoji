6265f0721531a143f5dc2290854c2542cc2a2121
Moricube <alex90656@gmail.com>
2020-06-10 10:44:11 +0700

    change dir

--- ng2-emoji.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="ng2-emoji" />
-export * from './lib/ng2-emoji.module';
-export * from './lib/ng2-emoji.pipe';
-export * from './lib/ng2-emoji.service';
