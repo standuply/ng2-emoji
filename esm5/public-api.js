ba7b64c56c59bcb10d517dc620afcae2c54c3505
Moricube <alex90656@gmail.com>
2020-06-10 11:29:26 +0700

    change dir

--- esm5/public-api.js
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ng2-emoji
- */
-export * from './lib/ng2-emoji.module';
-export * from './lib/ng2-emoji.pipe';
-export * from './lib/ng2-emoji.service';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljLWFwaS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL25nMi1lbW9qaS8iLCJzb3VyY2VzIjpbInB1YmxpYy1hcGkudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0dBRUc7QUFFSCxjQUFjLHdCQUF3QixDQUFDO0FBQ3ZDLGNBQWMsc0JBQXNCLENBQUM7QUFDckMsY0FBYyx5QkFBeUIsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIi8qXG4gKiBQdWJsaWMgQVBJIFN1cmZhY2Ugb2YgbmcyLWVtb2ppXG4gKi9cblxuZXhwb3J0ICogZnJvbSAnLi9saWIvbmcyLWVtb2ppLm1vZHVsZSc7XG5leHBvcnQgKiBmcm9tICcuL2xpYi9uZzItZW1vamkucGlwZSc7XG5leHBvcnQgKiBmcm9tICcuL2xpYi9uZzItZW1vamkuc2VydmljZSc7XG4iXX0=
